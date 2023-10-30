6bb24e8b0ca81c269f8c337159277981ab8a2afe
1lexaa <nice140@icloud.com>
2023-10-30 14:17:50 +0200

    menu

--- server.js
+++ server.js
@@ -1,70 +1,68 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const path = require('path');
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const path = require("path");
 
 var players = {};
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/menu', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'menu.html')); });
+app.get("/menu", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "menu.html"));
+});
 
-io.on('connection', (socket) => {
-  console.log('Пользователь подключился: ', socket.id);
+io.on("connection", (socket) => {
+  console.log("Пользователь подключился: ", socket.id);
 
-  
-    players[socket.id] = {
-      rotation: 0,
-      x: Math.floor(Math.random() * 700) + 50,
-      y: Math.floor(Math.random() * 500) + 50,
-      playerId: socket.id,
-      isMoving: false, // Добавляем информацию о состоянии движения
-      flipX: false, // Добавляем информацию о flipX
-    };
-    
-  
-  
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+    isMoving: false, // Добавляем информацию о состоянии движения
+    flipX: false, // Добавляем информацию о flipX
+  };
 
   // Отправить информацию о существующих игроках новому игроку
-  socket.emit('currentPlayers', players);
+  socket.emit("currentPlayers", players);
   // Отправить информацию о новом игроке другим игрокам
-  socket.broadcast.emit('newPlayer', players[socket.id]);
+  socket.broadcast.emit("newPlayer", players[socket.id]);
 
-  socket.on('disconnect', () => {
-    console.log('Пользователь отключился: ', socket.id);
+  socket.on("disconnect", () => {
+    console.log("Пользователь отключился: ", socket.id);
     delete players[socket.id];
-    io.emit('disconnect', socket.id);
+    io.emit("disconnect", socket.id);
   });
 
-  socket.on('playerMovement', (movementData) => {
+  socket.on("playerMovement", (movementData) => {
     players[socket.id].x = movementData.x;
     players[socket.id].y = movementData.y;
     players[socket.id].isMoving = movementData.isMoving;
     players[socket.id].flipX = movementData.flipX; // Обновляем flipX
     players[socket.id].animationKey = movementData.animationKey;
-  // Теперь отправляем информацию о движении и flipX всем клиентам, включая отправившего
-  io.emit('playerMoved', players[socket.id]);
+    // Теперь отправляем информацию о движении и flipX всем клиентам, включая отправившего
+    io.emit("playerMoved", players[socket.id]);
 
-  // Дополнительно отправьте информацию о flipX только другим клиентам (исключая отправившего)
-  socket.broadcast.emit('flipXUpdate', {
-    playerId: socket.id,
-    flipX: movementData.flipX,
-  });
-  // Отправляем информацию об анимации только другим клиентам (исключая отправившего)
-  socket.broadcast.emit('animationUpdate', {
-    playerId: socket.id,
-    animationKey: movementData.animationKey,
-  });
+    // Дополнительно отправьте информацию о flipX только другим клиентам (исключая отправившего)
+    socket.broadcast.emit("flipXUpdate", {
+      playerId: socket.id,
+      flipX: movementData.flipX,
+    });
+    // Отправляем информацию об анимации только другим клиентам (исключая отправившего)
+    socket.broadcast.emit("animationUpdate", {
+      playerId: socket.id,
+      animationKey: movementData.animationKey,
+    });
   });
 
   // Добавим обработку события "новый игрок" и отправку информации о нем текущему игроку
-  socket.on('newPlayer', () => {
-    socket.emit('currentPlayers', players);
+  socket.on("newPlayer", () => {
+    socket.emit("currentPlayers", players);
   });
 });
 
