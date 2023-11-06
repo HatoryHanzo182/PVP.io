741506f658b8acd89632b290d6ddea7ff39e83e6
1lexaa <nice140@icloud.com>
2023-11-06 21:27:39 +0200

    settings

--- public/js/menu.js
+++ public/js/menu.js
@@ -17,7 +17,6 @@ function InputValidityData(nick) {
 
 // == Chat container. ==
 function toggleChat() {
-
   var chatBox = document.getElementById("chat-box");
   chatBox.style.display = chatBox.style.display === "none" ? "block" : "none";
 }
@@ -102,8 +101,81 @@ function ConstructMessage(is, message)
 }
 // ==
 
-function toggleSettings() 
-{
+
+function sendMessage() {
+  var nickname = document.getElementById("input").value;
+  var message_input = document.getElementById("message");
+  const error_element = document.getElementById("error-input");
+  var message_text = message_input.value;
+
+  if (message_text.trim() === "") return;
+  if (!InputValidityData(nickname)) {
+    error_element.innerHTML = "To send a message, enter your nickname!";
+    return;
+  }
+
+  socket.emit("chatMessage", { user: nickname, text: message_text });
+
+  ConstructMessage("user-message", { user: nickname, text: message_text });
+
+  message_input.value = "";
+  error_element.innerHTML = "";
+}
+
+socket.on("chatMessage", (message) => {
+  ConstructMessage("received-message", message);
+});
+
+function ConstructMessage(is, message) {
+  switch (is) {
+    case "user-message": {
+      var chat_content = document.querySelector(".chat-content");
+      var message_element = document.createElement("div");
+
+      message_element.classList.add("user-message");
+
+      var user_nickname_element = document.createElement("span");
+
+      user_nickname_element.classList.add("user-nickname");
+      user_nickname_element.textContent = message.user + ":";
+
+      var message_text_element = document.createElement("span");
+
+      message_text_element.classList.add("message-text");
+      message_text_element.textContent = message.text;
+
+      message_element.appendChild(user_nickname_element);
+      message_element.appendChild(message_text_element);
+      chat_content.appendChild(message_element);
+      break;
+    }
+    case "received-message": {
+      var chat_content = document.querySelector(".chat-content");
+      var message_element = document.createElement("div");
+
+      message_element.classList.add("received-message");
+
+      var user_nickname_element = document.createElement("span");
+
+      user_nickname_element.classList.add("user-nickname");
+      user_nickname_element.textContent = message.user + ":";
+
+      var message_text_element = document.createElement("span");
+
+      message_text_element.classList.add("message-text");
+      message_text_element.textContent = message.text;
+
+      message_element.appendChild(user_nickname_element);
+      message_element.appendChild(message_text_element);
+      chat_content.appendChild(message_element);
+      break;
+    }
+  }
+}
+// ==
+
+
+function toggleSettings() {
   var settings = document.getElementById("settings-box");
   settings.style.display = settings.style.display === "none" ? "block" : "none";
 }
